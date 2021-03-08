d55ccf4d12eda41c9e482e86ffebaced241c7fce
Никита <Nikxdrummer@gmail.com>
2021-03-08 20:59:14 +0300

    fix urls vacancy

--- personalareaapp/urls.py
+++ personalareaapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 import personalareaapp.views as personalarea
+from Vacancy import views
 
 app_name = 'personalarea'
 
 urlpatterns = [
     path('', personalarea.IndexView.as_view(extra_context={'title': 'Личный кабинет'}), name='view'),
+
+    path('vacancy/create/', views.VacancyCreateView.as_view(), name='vacancy_create'),
+    path('vacancy/update/<int:pk>/', views.VacancyUpdateView.as_view(), name='vacancy_update'),
+    path('vacancy/delete/<int:pk>/', views.VacancyDeleteView.as_view(), name='vacancy_delete'),
 ]
