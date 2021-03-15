ebe8373d126e01e76d48c7650ecc353feeb363ec
kopylovdk <kopylovdk@gmail.com>
2021-03-15 11:10:20 +0300

    Добавил бат, очищающий все миграции и БД, а затем создающий все занового. Запускается также run.bat , если не хотите очищать БД - подправьте файл run.bat , старый код закомментирован.

---
+++ mainapp/management/commands/clear_and_migrate.py
@@ -0,0 +1,31 @@
+from django.core.management import BaseCommand
+import os
+# import subprocess
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        current_path = os.getcwd()
+        apps_to_clear_migrations = ['applicantapp', 'authapp', 'mainapp', 'personalareaapp', 'resume', 'Vacancy']
+
+        for app in apps_to_clear_migrations:
+            path = os.path.join(current_path, f'{app}\\migrations')
+            print(f'Миграции для {app} очищены.')
+            self.remove_files(path)
+
+        # self.remove_files(os.path.join(current_path, 'Documents_folder\\docs'))
+        db_path = os.path.join(current_path, 'db.sqlite3')
+        if os.path.exists(db_path):
+            os.remove(db_path)
+            print('База данны удалена.\n')
+
+        print(os.popen('py manage.py makemigrations').read())
+        print(os.popen('py manage.py migrate').read())
+
+    def remove_files(self, path):
+        for file in os.listdir(path):
+            filepath = os.path.join(path, file)
+            if file != '__init__.py' and '.gitkeep' \
+                    and os.path.isfile(filepath):
+                os.remove(filepath)
