22a8d86a9307e47ac955f5662bd13aa62a7e629b
Lyubov Golubeva <golubeva.lyubov@gmail.com>
2021-03-22 00:41:23 +0300

    blogposts

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(verbose_name='Заголовок', max_length=50, null=False)
+    text = models.TextField(verbose_name='Новость')
+    date = models.DateTimeField(verbose_name='Дата публикации', auto_now=True)
+
+    class Meta:
+        verbose_name = 'новостной пост'
+        verbose_name_plural = 'новостные посты'
+
+    def __str__(self):
+        return self.title
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-
+from mainapp.models import BlogPost
 from vacancyapp.models import Vacancy
 from resumeapp.models import Resume
 
 
 def main_list(request):
+    blog_posts = BlogPost.objects.all()
     if request.user.is_staff:
         data = Resume.objects.filter(is_draft=False)
         # data = Resume.objects.all()
@@ -16,5 +17,6 @@ def main_list(request):
         title = 'Список вакансий'
     context = {
         'title': title,
-        'data': data}
+        'data': data,
+        'blog_posts': blog_posts}
     return render(request, 'mainapp/main.html', context)
