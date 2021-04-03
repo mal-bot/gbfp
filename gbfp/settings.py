e34f3051f9bff47bdaee9774ff6667d17599522a
nikdestrave <Nikxdrummer@gmail.com>
2021-04-03 11:05:11 +0300

    block of partners

--- gbfp/settings.py
+++ gbfp/settings.py
@@ -135,3 +135,18 @@ STATICFILES_DIRS = (
 )
 MEDIA_URL = '/documents_folder/'
 MEDIA_ROOT = BASE_DIR / MEDIA_URL[1:-1]
+
+DOMAIN_NAME = 'http://localhost:8000'
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '25'
+EMAIL_HOST_USER = 'gb@work.local'
+EMAIL_HOST_PASSWORD = 'gb'
+EMAIL_USE_SSL = False
+
+# вариант python -m smtpd -n -c DebuggingServer localhost:25
+# EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
+
+# вариант логирования сообщений почты в виде файлов вместо отправки
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = 'tmp/email-messages/'
